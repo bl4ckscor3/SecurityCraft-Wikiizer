eb2ce467deaaa788221e56bd091eaebd9d1d79e8
bl4ckscor3 <bl4ckscor33@gmail.com>
2022-07-08 21:39:09 +0200

    fix compatibility with Forge 41.0.64+

--- src/main/java/bl4ckscor3/wikiizer/SecurityCraftWikiizer.java
+++ src/main/java/bl4ckscor3/wikiizer/SecurityCraftWikiizer.java
@@ -4,7 +4,7 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.ChatScreen;
 import net.minecraft.network.chat.Component;
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.client.event.ScreenEvent.InitScreenEvent;
+import net.minecraftforge.client.event.ScreenEvent;
 import net.minecraftforge.client.gui.widget.ExtendedButton;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -14,7 +14,7 @@ import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 @EventBusSubscriber(modid = "scwikiizer", value = Dist.CLIENT)
 public class SecurityCraftWikiizer {
 	@SubscribeEvent
-	public static void onInitScreen(InitScreenEvent event) {
+	public static void onInitScreen(ScreenEvent.Init event) {
 		if (event.getScreen() instanceof ChatScreen screen)
 			screen.addRenderableWidget(new ExtendedButton(5, 5, 20, 20, Component.literal("W"), b -> Minecraft.getInstance().setScreen(new WikiizerScreen())));
 	}
