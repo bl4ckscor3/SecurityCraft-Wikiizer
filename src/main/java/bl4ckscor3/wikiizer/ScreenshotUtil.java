dbe256b3942191a92e175caf210b53144b31748e
bl4ckscor3 <bl4ckscor33@gmail.com>
2025-05-01 22:27:24 +0200

    Fix crash and update SecurityCraft

--- src/main/java/bl4ckscor3/wikiizer/ScreenshotUtil.java
+++ src/main/java/bl4ckscor3/wikiizer/ScreenshotUtil.java
@@ -11,6 +11,7 @@ import javax.imageio.stream.ImageOutputStream;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import com.mojang.blaze3d.pipeline.RenderTarget;
 import com.mojang.blaze3d.platform.NativeImage;
 
 import net.minecraft.Util;
@@ -23,54 +24,58 @@ public class ScreenshotUtil {
 	private ScreenshotUtil() {}
 
 	public static void grabScreenshot(File target) {
-		NativeImage img = new NativeImage(0, 0, false);
-		int startX = -1;
-		int lastGreenX = -1;
-		int startY = -1;
-		int lastGreenY = -1;
+		RenderTarget framebuffer = Minecraft.getInstance().getMainRenderTarget();
+		NativeImage img = new NativeImage(framebuffer.width, framebuffer.height, false);
 
 		//takes a screenshot of the whole game screen
-		Screenshot.takeScreenshot(Minecraft.getInstance().getMainRenderTarget(), img::copyFrom);
+		Screenshot.takeScreenshot(framebuffer, i -> {
+			int startX = -1;
+			int lastGreenX = -1;
+			int startY = -1;
+			int lastGreenY = -1;
 
-		//the crafting grid texture has green corners. for one, to determine the area of the crafting grid (which is the only part of the image that is needed),
-		//but also so the corners can be removed and the resulting image will be fully transparent in those places
-		for (int x = 0; x < img.getWidth(); x++) {
-			for (int y = 0; y < img.getHeight(); y++) {
-				if (img.getPixel(x, y) == 0xFF00FF00) {
-					if (startX == -1 && startY == -1) {
-						startX = x;
-						startY = y;
-					}
-					else {
-						lastGreenX = x;
-						lastGreenY = y;
-					}
+			img.copyFrom(i);
 
-					img.setPixel(x, y, 0x00000000);
+			//the crafting grid texture has green corners. for one, to determine the area of the crafting grid (which is the only part of the image that is needed),
+			//but also so the corners can be removed and the resulting image will be fully transparent in those places
+			for (int x = 0; x < img.getWidth(); x++) {
+				for (int y = 0; y < img.getHeight(); y++) {
+					if (img.getPixel(x, y) == 0xFF00FF00) {
+						if (startX == -1 && startY == -1) {
+							startX = x;
+							startY = y;
+						}
+						else {
+							lastGreenX = x;
+							lastGreenY = y;
+						}
+
+						img.setPixel(x, y, 0x00000000);
+					}
 				}
 			}
-		}
 
-		NativeImage copy = new NativeImage(lastGreenX - startX + 1, lastGreenY - startY + 1, false);
+			NativeImage copy = new NativeImage(lastGreenX - startX + 1, lastGreenY - startY + 1, false);
 
-		//copy the area with the crafting grid to a new native image to save it
-		for (int x = startX; x <= lastGreenX; x++) {
-			for (int y = startY; y <= lastGreenY; y++) {
-				copy.setPixel(x - startX, y - startY, img.getPixel(x, y));
+			//copy the area with the crafting grid to a new native image to save it
+			for (int x = startX; x <= lastGreenX; x++) {
+				for (int y = startY; y <= lastGreenY; y++) {
+					copy.setPixel(x - startX, y - startY, img.getPixel(x, y));
+				}
 			}
-		}
 
-		Util.ioPool().execute(() -> {
-			try {
-				copy.writeToFile(target);
-			}
-			catch (Exception exception) {
-				LOGGER.warn("Couldn't save screenshot", exception);
-			}
-			finally {
-				img.close();
-				copy.close();
-			}
+			Util.ioPool().execute(() -> {
+				try {
+					copy.writeToFile(target);
+				}
+				catch (Exception exception) {
+					LOGGER.warn("Couldn't save screenshot", exception);
+				}
+				finally {
+					img.close();
+					copy.close();
+				}
+			});
 		});
 	}
 
